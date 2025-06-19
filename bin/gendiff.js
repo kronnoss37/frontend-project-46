a0592382c8dbca08cd6e9d1cb1e0e18bfc540500
kronnoss <kronnoss.it@gmail.com>
2025-06-19 19:14:48 +0300

    Update gendiff with new arguments and option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,5 +2,10 @@
 
 import { program } from 'commander';
 
-program.name('gendiff').description('Compares two configuration files and shows a difference.').version('1.0.0');
+program
+	.name('gendiff')
+	.description('Compares two configuration files and shows a difference.')
+	.version('1.0.0')
+	.arguments('<filepath1> <filepath2>')
+	.option('-f, --format [type]', 'output format');
 program.parse();

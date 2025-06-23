383dab85765daecb02be999bc95074a2bc42bcf4
kronnoss <kronnoss.it@gmail.com>
2025-06-23 17:21:12 +0300

    Add entry point index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+// import parseFile from './src/fileParser.js'
+// import genDiff from 'filePath'
+
+// export default genDiff
+
+// Здесь будет точка входа нашего проекта как библиотеки, будем импортировать функции из файла где содержится логика, которая возвращает разницу между двумя файлами

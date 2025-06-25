93ef81872f9dd486b94f09047d47717c161c03ee
kronnoss <kronnoss.it@gmail.com>
2025-06-25 19:40:01 +0300

    Fix mistakes with eslint

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,22 +1,22 @@
 #!/usr/bin/env node
 
-import { program } from 'commander';
+import { program } from 'commander'
 import parseFile from '../src/fileParser.js'
 import compare from '../src/filesComparison.js'
 
 program
-	.name('gendiff')
-	.description('Compares two configuration files and shows a difference.')
-	.version('1.0.0')
-	.arguments('<filepath1> <filepath2>')
-	.option('-f, --format [type]', 'output format')
-	.action((filepath1, filepath2, options) => {
-    const firstFileData = parseFile(filepath1);
-    const secondFileData = parseFile(filepath2);
-    const result = compare(firstFileData, secondFileData);
-		// console.log(`file1: ${JSON.stringify(firstFileData)}`);
-		// console.log(`file2: ${JSON.stringify(secondFileData)}`);
-    console.log(result);
-	});
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format')
+  .action((filepath1, filepath2) => { // options
+    const firstFileData = parseFile(filepath1)
+    const secondFileData = parseFile(filepath2)
+    const result = compare(firstFileData, secondFileData)
+    // console.log(`file1: ${JSON.stringify(firstFileData)}`);
+    // console.log(`file2: ${JSON.stringify(secondFileData)}`);
+    console.log(result)
+  })
 
-program.parse();
+program.parse()
--- src/fileParser.js
+++ src/fileParser.js
@@ -1,13 +1,14 @@
-//forParsing
+// forParsing
 
-import fs from 'fs';
-import path from "path";
+import fs from 'fs'
+import path from 'path'
 
-const getFileFormat = (filePath) => filePath.split('.').at(-1);
+const getFileFormat = filePath => filePath.split('.').at(-1)
 
 export default (filePath) => {
-  const fileFormat = getFileFormat(filePath);
+  // const fileFormat = getFileFormat(filePath)
+  getFileFormat(filePath)
   // console.log(fileFormat);
-  const fileData = fs.readFileSync(path.resolve(filePath));
-  return JSON.parse(fileData);
-};
+  const fileData = fs.readFileSync(path.resolve(filePath))
+  return JSON.parse(fileData)
+}

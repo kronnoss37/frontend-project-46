819ca5321264bee63197e66af7411700928a4bed
kronnoss <kronnoss.it@gmail.com>
2025-06-27 18:29:31 +0300

    Update eslint.config for jest

--- eslint.config.js
+++ eslint.config.js
@@ -2,9 +2,23 @@ import js from '@eslint/js'
 import globals from 'globals'
 import { defineConfig } from 'eslint/config'
 import stylistic from '@stylistic/eslint-plugin'
+import jest from 'eslint-plugin-jest'
 
 export default defineConfig([
   stylistic.configs.recommended,
-  { files: ['**/*.{js,mjs,cjs}'], plugins: { js }, extends: ['js/recommended'] },
-  { files: ['**/*.{js,mjs,cjs}'], languageOptions: { globals: globals.node } },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    languageOptions: { globals: globals.node },
+  },
+  {
+    files: ['**/*.test.js'],
+    plugins: { jest },
+    extends: ['jest/recommended'],
+    languageOptions: { globals: globals.jest },
+  },
 ])

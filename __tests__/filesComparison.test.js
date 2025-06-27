573c5d159f7772035829f5e212ad5afb38acdaf5
kronnoss <kronnoss.it@gmail.com>
2025-06-27 23:50:44 +0300

    Update test file filesComparison.test

--- __tests__/filesComparison.test.js
+++ __tests__/filesComparison.test.js
@@ -8,10 +8,9 @@ const __dirname = path.dirname(__filename)
 
 const getFilePath = fileName => path.join(__dirname, '..', '__fixtures__', fileName)
 
-test('compare files', () => {
-  const firstFile = parseFile(getFilePath('file1.json'))
-  const secondFile = parseFile(getFilePath('file2.json'))
-  const result = `{
+let result
+beforeAll(() => {
+  result = `{
   - follow: false
     host: hexlet.io
   - proxy: 123.234.53.22
@@ -19,8 +18,26 @@ test('compare files', () => {
   + timeout: 20
   + verbose: true
 }`
+})
+
+test('compare json files', () => {
+  const firstFile = parseFile(getFilePath('file1.json'))
+  const secondFile = parseFile(getFilePath('file2.json'))
 
   expect(compare(firstFile, secondFile)).toEqual(result)
+})
+
+test('compare yml files', () => {
+  const firstFile = parseFile(getFilePath('file1.yml'))
+  const secondFile = parseFile(getFilePath('file2.yml'))
+
+  expect(compare(firstFile, secondFile)).toEqual(result)
+})
+
+test('compare empty objects', () => {
   expect(compare({}, {})).toEqual(`{\n\n}`)
+})
+
+test('compare if param is not a plain object', () => {
   expect(() => compare({}, null)).toThrow()
 })

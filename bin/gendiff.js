fa5354103aa8d05d72f8711976b96f6258a8c0a2
kronnoss <kronnoss.it@gmail.com>
2025-07-03 16:58:05 +0300

    Update file bin/gendiff with new option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,19 +2,19 @@
 
 import { program } from 'commander'
 import parseFile from '../src/fileParser.js'
-import compare from '../src/filesComparison.js'
+import gendiff from '../src/filesComparison.js'
 
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format [type]', 'output format')
-  .action((filepath1, filepath2) => { // options
+  .option('-f, --format [type]', 'output format', 'stylish')
+  .action((filepath1, filepath2, options) => {
     try {
       const firstFileData = parseFile(filepath1)
       const secondFileData = parseFile(filepath2)
-      const filesDifferrence = compare(firstFileData, secondFileData)
+      const filesDifferrence = gendiff(firstFileData, secondFileData, options.format)
       console.log(filesDifferrence)
     }
     catch (error) {

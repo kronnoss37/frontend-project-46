68cf59f06dc853f748f2ec1a30f682bcc297cdb6
kronnoss <kronnoss.it@gmail.com>
2025-07-04 19:20:29 +0300

    Update file formatters/index.js with json format

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,13 +1,15 @@
 import stylish from './stylish.js'
 import plain from './plain.js'
+import json from './json.js'
 
 const getFunForFormatting = (format) => {
   if (format === 'stylish') return stylish
   if (format === 'plain') return plain
+  if (format === 'json') return json
 }
 
 export default (data, format) => {
   const formatData = getFunForFormatting(format)
-  if (!formatData) throw new Error(`Format "${format}" is invalid. Supported formats: stylish!`)
+  if (!formatData) throw new Error(`Format "${format}" is invalid. Supported formats: stylish, plain and json!`)
   return formatData(data)
 }
